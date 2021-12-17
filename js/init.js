c3b52e8a5d57e0e31910e8c0ca8a2f192359ae17
SpendersShop1 <spenders.shop@gmail.com>
2021-12-17 15:25:24 -0600

    cambio

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyCyUjDfb006cwQL-M5Lec7z9ewyn3ay1Dk",
     authDomain: "spenders-shop.firebaseapp.com",
     projectId: "spenders-shop",
@@ -8,5 +8,5 @@ const firebaseConfig = {
     messagingSenderId: "459637118632",
     appId: "1:459637118632:web:04123b04ce2894623c949d",
     measurementId: "G-P93YVNRES0"
-  };
+  });
   
